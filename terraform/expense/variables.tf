6c159e861f0e45625bb22fe9e90a947ddfb019c3
Vijay Guntireddy <vijay.guntireddy@gmail.com>
2024-09-12 05:33:37 +0530

    ansible

---
+++ terraform/expense/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_names" {
+  type = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "DEV"
+        terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    type = string
+    default = "Z07904242N80PGXSVPTJN"
+}
